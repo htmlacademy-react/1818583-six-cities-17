7de65a811e366112049e980d9fd87fe7c084b2b8
Oleg Verushkin <reaktorGithub@yandex.ru>
2024-12-17 10:51:58 +0300

    Контроль и ограничения (часть 2)

---
+++ src/hooks/use-offers.ts
@@ -0,0 +1,35 @@
+import {useAppSelector} from './useAppSelector.ts';
+import {filterOffersByCity, getSortedOffers} from '../adaptors.ts';
+import {LocationType, OfferType, Point} from '../types.ts';
+import {DEFAULT_CITY} from '../const.ts';
+
+type ReturnOffers = {
+  offers: OfferType[];
+  points: Point[];
+  city: LocationType;
+}
+
+function useOffers(): ReturnOffers {
+  const cityId = useAppSelector((state) => state.city);
+  const offers = useAppSelector((state) => state.offers);
+  const sortBy = useAppSelector((state) => state.sortOffersBy);
+
+  const filteredOffers = filterOffersByCity(offers, cityId);
+
+  const points: Point[] = filteredOffers.map((offer) => ({
+    id: offer.id,
+    location: offer.location,
+  }));
+
+  const city: LocationType = filteredOffers.length > 0 ? filteredOffers[0].city.location : DEFAULT_CITY;
+
+  const sortedOffers = getSortedOffers(filteredOffers, sortBy);
+
+  return {
+    offers: sortedOffers,
+    points,
+    city,
+  };
+}
+
+export {useOffers};
